e93e4bab5ac6d538f099d25f073f70b5adb869c2
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-04-13 10:58:06 -0500

    Config Firebase

---
+++ src/firebase/Config.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyALiH_VQF3JVn0s-vyRLPaR8ndLqjDypJA',
+  authDomain: 'finance-tracker-85380.firebaseapp.com',
+  projectId: 'finance-tracker-85380',
+  storageBucket: 'finance-tracker-85380.appspot.com',
+  messagingSenderId: '658776356728',
+  appId: '1:658776356728:web:d2197c78790f950b3a3a8d',
+  measurementId: 'G-YGP4J9GQKG',
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+
+export { projectFirestore };
