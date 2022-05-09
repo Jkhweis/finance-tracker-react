d2abdc690432d79f10a2a90180e0ca74c473925c
JoyKhweis <68160623+Jkhweis@users.noreply.github.com>
2022-05-08 22:16:26 -0500

    Using the useFirestore Hook

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,12 +1,17 @@
-import styles from './Home.module.css';
 import TransactionForm from './TransactionForm';
+import { useAuthContext } from '../../hooks/useAuthContext';
+
+//styles
+import styles from './Home.module.css';
 
 export default function Home() {
+  const { user } = useAuthContext();
+
   return (
     <div className={styles.container}>
       <div className={styles.content}>Transaction list</div>
       <div className={styles.sidebar}>
-        <TransactionForm />
+        <TransactionForm uid={user.uid} />
       </div>
     </div>
   );
